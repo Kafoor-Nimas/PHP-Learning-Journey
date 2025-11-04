df2ffd62ee8ca2370a08d132a56a29f4fa364fdb
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-04 11:43:33 +0530

    first PHP

---
+++ 01.Basic.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //This is a single line comment
+        echo "<h1>This is my First PHP project</h1>"
+        /*
+        This is a
+        Multiple line 
+        Comment
+        */
+    ?>
+</body>
+</html>
---
+++ 02.mixing.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mixing PHP with HTML</title>
+    <style>
+        table{
+            boder:1px solid #ccc;
+            border-collapse:collapse;
+        }
+        td,th{
+            border:1px solid #ccc;
+            padding:5px 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Mixing PHP with HTML</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Ram</td>
+                <td>23</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
