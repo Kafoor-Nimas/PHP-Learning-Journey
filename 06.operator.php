512170440d788fcdd03471a7a09df121dd41feae
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-05 08:02:21 +0530

    Comparison operator & Spaceship operator

--- 06.operator.php
+++ 06.operator.php
@@ -8,4 +8,24 @@ echo "<br>".gettype($a);
 $a=40;
 echo "<br> A Value : ".$a;
 echo "<br> A Value : ".-$a;
+
+# Comparison Operator
+echo "<br>";
+$a=45;
+$b="45";
+var_dump($a==$b);
+echo "<br>";
+var_dump($a===$b);
+echo "<br>";
+var_dump($a!=$b);
+echo "<br>", var_dump($a<>$b) ;
+echo "<br>";
+echo "<br>", var_dump($a!==$b) ;
+
+//Spaceship
+$a=5;
+$b=25;
+echo "<br>", var_dump($a<=>$b) ;
+
+
 ?>
