7390d95c7609d486a2c4955450275cf9aa401e45
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-05 09:28:33 +0530

    increment and decrement

--- 06.operator.php
+++ 06.operator.php
@@ -37,9 +37,37 @@ echo "<br>", var_dump($eng>=35 and $mat>=35 and $sci>=35);
 echo "<br>", var_dump($eng>=35 or $mat>=35 or $sci>=35);
 echo "<br>", var_dump($eng>=35 && $mat>=35 && $sci>=35);
 echo "<br>", var_dump($eng>=35 || $mat>=35 || $sci>=35);
-echo "<br/><br/>" , var_dump(!$eng>=35)
+echo "<br/><br/>" , var_dump(!$eng>=35);
+
+//Error control opeartor
+echo "<br>";
+echo "<br>";
+// $d=10;
+$b=20;
+$c=@$d+$b;
+echo "Total : $c";
 
 
+echo "<br>";
+
+$char='A';
+echo $char.'<br/>';
+echo ++$char."<br>";
+$char='c';
+echo $char.'<br/>';
+echo ++$char."<br>";
+
+$rollNo="A0";
+echo $rollNo++."<br/>";
+echo ++$rollNo."<br/>";
+echo ++$rollNo."<br/>";
+
+$regNo="A07";
+echo ++$regNo."<br/>";
+echo ++$regNo."<br/>";
+echo ++$regNo."<br/>";
+echo --$regNo."<br/>"; //decrement won't affect strings
+echo --$regNo."<br/>";
 
 
 ?>
